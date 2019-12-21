bc92581c9099a77d4ec50714b0d512a608c74d78
Roman Pobihushka <pobihushka.roman@gmail.com>
2019-12-21 19:23:06 +0200

    add ssh(fix)

--- src/main/java/ru/javawebinar/topjava/HttpsEnforcer.java
+++ src/main/java/ru/javawebinar/topjava/HttpsEnforcer.java
@@ -21,6 +21,7 @@ public class HttpsEnforcer implements Filter {
             if (request.getHeader(X_FORWARDED_PROTO).indexOf("https") != 0) {
                 String pathInfo = (request.getPathInfo() != null) ? request.getPathInfo() : "";
                 response.sendRedirect("https://" + request.getServerName() + pathInfo);
+                return;
             }
         }
         filterChain.doFilter(request, response);
